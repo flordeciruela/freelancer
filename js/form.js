e1f272da02d3741b6538a4343e9e7eb6c66a62c8
Wendy Reyes <wendy.reyessan@gmail.com>
2017-05-10 14:09:59 -0500

    validation

---
+++ js/form.js
@@ -0,0 +1,49 @@
+
+var input = document.getElementsByTagName('input');
+
+var submit = input[3];
+var formulario = document.getElementsByTagName('form');
+
+submit.addEventListener('click',function(event) {
+  event.preventDefault();
+
+		if(input[0].value.length !=0 && input[1].value.length !=0 &&
+			 input[2].value.length !=0) {
+      formulario.reset();  // limpiar formulario
+		}
+})
+
+input[0].addEventListener("blur", function(){
+	var nTest = /^([A-ZÁ-Ú]{1}[a-zá-úA-ZÁ-Ú]+[\s]*)+$/g;
+	if(!nTest.test(input[0].value)) {
+		input[0].nextElementSibling.innerText = "Ingresar datos con primera letra mayúscula";
+	} else {
+		input[0].nextElementSibling.innerText = "";
+	}
+});
+
+input[1].addEventListener("blur", function(){
+	var emailTest = /([a-zA-Z0–9]+)([\_\.\-{1}])?([a-zA-Z0–9]+)\@([a-zA-Z0–9]+)([\.])([a-zA-Z\.]+)/g;
+	if(!emailTest.test(input[1].value)) {
+		input[1].nextElementSibling.innerText = "Email no válido";
+	} else {
+		input[1].nextElementSibling.innerText = "";
+	}
+});
+
+input[2].addEventListener("blur", function(){
+	var phoneTest = /^([0-9])*$/;
+	if(!phoneTest.test(input[2].value)) {
+		input[2].nextElementSibling.innerText = "teléfono no válido";
+	} else {
+		input[2].nextElementSibling.innerText = "";
+	}
+});
+
+input[3].addEventListener("blur", function(){
+	if(input[3].value.length == 0) {
+		input[3].nextElementSibling.innerText = "Ingresar comentario";
+	} else {
+		input[3].nextElementSibling.innerText = "";
+	}
+});
